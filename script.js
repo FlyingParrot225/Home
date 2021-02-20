70db16a81f9123ad661c85efef51711c34310e40
FlyingParrot225 <61127422+FlyingParrot225@users.noreply.github.com>
2021-02-20 11:33:56 -0500

    commit

---
+++ script.js
@@ -0,0 +1,4 @@
+if(window.innerWidth < 786)
+{
+    alert("This website is under development and not currently able to be correctly shown on a device with a width that is less than 786 pixels. For a better experience, please use a wider device.");
+}
